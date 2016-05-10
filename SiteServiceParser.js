fb2d262253be0cae73ea3e8707a0f90b36525eb8
DmitryKlokov <dmitryklokov95@gmail.com>
2016-05-10 12:23:14 +0300

    Clear all collection in parser

--- SiteServiceParser.js
+++ SiteServiceParser.js
@@ -5,6 +5,8 @@ var ServiceMass = [];
 exports.parse = function parse(fn) {
     request('http://orgpage.by/belarus/%D0%B0%D0%B2%D1%82%D0%BE%D1%81%D0%B5%D1%80%D0%B2%D0%B8%D1%81%D1%8B/', function(error, response, html) {
         if (!error && response.statusCode == 200) {
+           mass = [];
+           ServiceMass = [];
             var $ = cheerio.load(html);
 
             $('#main_div > div.main > div.wrapper > div.main_left > div.r_firms.nomargin > div.firm').each(function(i, element) {
