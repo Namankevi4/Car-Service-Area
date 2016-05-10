7ad6d65d70384155c8391a60fd50ec6e7f7619c5
DmitryKlokov <dmitryklokov95@gmail.com>
2016-05-10 12:12:17 +0300

    Create waterfall, setInterval

--- CreateDB.js
+++ CreateDB.js
@@ -4,34 +4,38 @@ var async = require('async');
 var ParseModule = require('./SiteServiceParser');
 //mongoose.set('debug',true);//выводит в консоль все действия
 
-async.series([
-  open,
-  dropDatabase,
-  createServices,
-  close
-],function(err, result){
-  console.log(arguments);
-});
+setInterval(function(){
+  async.waterfall([
+    open,
+    Parse,
+    CreateServices,
+    close
+  ], function(err, result){
+    console.log(arguments);
+  });
+},60000);
 
 function open(callback){
   mongoose.connection.on('open',callback);
+  callback();
 }
-function dropDatabase(callback){
-   var db = mongoose.connection.db;
-   db.dropDatabase(callback);
-}
-function createServices(callback){
-  
-  var Parsedata = [];
+function Parse(callback){
   ParseModule.parse(function(data) {
-        console.log(data);
-        Parsedata = data;
-        async.each(data, function(ServiceData, callback){
-          var service = new Service(ServiceData);
-          service.save(callback);
-        }, callback)
-  }); 
+    console.log(data.length);
+    callback(null, data);
+  });
+}
+function CreateServices(ParseData, callback){
+  if(ParseData.length>0){
+    var db = mongoose.connection.db;
+    db.dropDatabase();
+    async.each(ParseData, function(ServiceData, callback){
+      var service = new Service(ServiceData);
+      service.save(callback);
+    })
+  }
 }
 function close(callback){
-  mongoose.disconnect(callback);
+  mongoose.disconnect();
+  callback();
 }
